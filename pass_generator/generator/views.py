84daa9f18c9456398249aa88186d443302d686db
mack <hypermail@yandex.ru>
2022-11-16 18:06:57 +0300

    main_about_page

--- pass_generator/generator/views.py
+++ pass_generator/generator/views.py
@@ -9,9 +9,24 @@ def password(request):
 
     thepassword = ''
     characters = list('abcdefghijklmnopqrstuvwxyz')
-    lenth = 10
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    if request.GET.get('specialCharacter'):
+        characters.extend(list('!@#$%^&*()_+'))
+
+    lenth = int(request.GET.get('length', 10))
 
     for i in range(lenth):
         thepassword += random.choice(characters)
 
     return render(request, 'generator/password.html', {'password': thepassword})
+
+def about(request):
+    params = request.GET.get('?', '')
+    # print(params)
+    return render(request, 'generator/about.html')
